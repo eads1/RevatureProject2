15969b2ed8e557757db2e2cc4ca64c03a64eed98
Gao Pan <gaopan1996@gmail.com>
2018-07-17 01:52:58 -0400

    Added email functionality and user services

---
+++ Project2/src/main/java/com/project/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.project.service;
+
+import com.project.dao.UserDao;
+import com.project.model.User;
+
+public class UserServiceImpl implements UserService {
+
+	private static UserDao userDao= new UserDao();
+	
+	@Override
+	public int insertUser(User user) {
+		userDao.insert(user);
+		return 0;
+	}
+	
+	@Override
+	public User getUserByEmail(String email) {
+		return userDao.selectUserByEmail(email);
+	}
+	
+}
