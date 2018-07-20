aee36045bcd0ac3434aa4a77dce6f13addb215c9
Kea Uehara <keal@hawaii.edu>
2018-07-20 08:17:29 -0400

    Didn't really do anything here

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
+
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,10 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
