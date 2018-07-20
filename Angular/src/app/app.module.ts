a8c5245d8e8ea1c0bd620ddca2ab5763affdc5fb
Kea <KeaUehara@gmail.com>
2018-07-20 08:18:25 -0400

    meh

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,10 +10,11 @@ import { LoginComponent } from './login/login.component';
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
