e20e77a97c147af98db098e6dfccbd7558ced1cd
davidyang11 <yangd5987@gmail.com>
2018-07-22 19:43:30 -0400

    Finished setting up the user service, and dao for updaing profile

--- Project2/src/main/java/com/project/controller/UpdateController.java
+++ Project2/src/main/java/com/project/controller/UpdateController.java
@@ -7,4 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class UpdateController {
 
+	public UpdateController() {
+		
+	}
+	
+	//
 }
--- Project2/src/main/java/com/project/dao/UserDao.java
+++ Project2/src/main/java/com/project/dao/UserDao.java
@@ -22,8 +22,18 @@ public class UserDao {
 
 	public User selectUserByEmail(String email) {
 		User user = sessFact.getCurrentSession().createQuery("FROM User WHERE email = :email", User.class).setParameter("email", email).uniqueResult();
-		
 		return user;
 	}
 
+	public int updateUserProfile(User user) {
+		int status = 0;
+		status = sessFact.getCurrentSession().createQuery("UPDATE User SET email = :email, SET fname = :fname, SET lname = :lname"+
+		" WHERE user_id = :user_id", User.class)
+				.setParameter("email",user.getEmail())
+				.setParameter("fname",user.getFname())
+				.setParameter("lname",user.getLname())
+				.setParameter("user_id",user.getUserId())
+				.executeUpdate();
+		return status;
+	}
 }
