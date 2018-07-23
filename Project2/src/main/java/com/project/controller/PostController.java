9145a5acf437593993568f807b55b0cb458fdae5
eads1 <meads30@gmail.com>
2018-07-23 11:01:45 -0400

    Removing Head problem

--- Project2/src/main/java/com/project/controller/PostController.java
+++ Project2/src/main/java/com/project/controller/PostController.java
@@ -1,10 +1,7 @@
 package com.project.controller;
 
-<<<<<<< HEAD
-=======
 import java.util.HashMap;
 import java.util.List;
->>>>>>> dev
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
