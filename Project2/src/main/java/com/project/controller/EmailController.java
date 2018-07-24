1f18dbebd2632c80a5c37d386f1246d4ea9c61be
davidyang11 <yangd5987@gmail.com>
2018-07-23 20:52:01 -0400

    cut out some unneeded code in maven

--- Project2/src/main/java/com/project/controller/EmailController.java
+++ Project2/src/main/java/com/project/controller/EmailController.java
@@ -1,13 +1,7 @@
 package com.project.controller;
 
-import org.simplejavamail.email.Email;
-import org.simplejavamail.email.EmailBuilder;
-import org.simplejavamail.mailer.Mailer;
-import org.simplejavamail.mailer.MailerBuilder;
-import org.simplejavamail.mailer.config.TransportStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.service.ResetService;
