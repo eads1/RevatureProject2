6044fd049bff83b930e6924ac6546516d2255c83
davidyang11 <yangd5987@gmail.com>
2018-07-24 05:38:29 -0400

    GOt the damn emailing finally working!

--- Project2/src/main/java/com/project/controller/EmailController.java
+++ Project2/src/main/java/com/project/controller/EmailController.java
@@ -23,25 +23,21 @@ public class EmailController {
 	}
 	
 	@PostMapping(value = "/reset.do")
-	public Map<String, Boolean> reset(String email) {
-		User tempUser = resetService.checkEmail(email);
-		System.out.println("Email: " + email);
+	public Map<String, Boolean> reset(String inputEmail) {
+		User tempUser = resetService.checkEmail(inputEmail);
 		//check if there is a user with the inputted email
 		if (tempUser != null) {
 			//generate new password
 			String password = resetService.generatePassword();
 			//hash it
-			System.out.println("Password: " + password);
 			String newPassword = resetService.hashPassword(password);
-			System.out.println("New Password: " + newPassword);
 			//update user account with new email
 			tempUser.setPassword(newPassword);
-			System.out.println("TempUser Password: " + tempUser.getPassword());
 			resetService.updateUserEmail(tempUser);
 			//send email
 			String body = "Your reset password is " + password + ". So please treat it with care from now on.\n" +
 			"Thank you very much.";
-			resetService.sendFromGMail(tempUser.getFname() + " " + tempUser.getFname(), email, "New Password", body);
+			resetService.sendFromGMail(tempUser.getFname() + " " + tempUser.getFname(), inputEmail, "New Password", body);
 			//return success
 			return Collections.singletonMap("success", true);
 		}
