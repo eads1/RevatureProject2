0277511fc51ac31ea4c4793b1da659ef9aff4132
eads1 <meads30@gmail.com>
2018-07-25 13:16:34 -0400

    logging login

--- Project2/src/main/java/com/project/aspect/Project2Aspect.java
+++ Project2/src/main/java/com/project/aspect/Project2Aspect.java
@@ -17,7 +17,7 @@ import com.project.model.User;
 @Component("aspect")
 public class Project2Aspect {
 	
-	Logger logger = Logger.getLogger(Project2Aspect.class);
+	static Logger logger = Logger.getLogger(Project2Aspect.class);
 	
 	@Autowired
 	SessionFactory sessFact;
@@ -26,11 +26,6 @@ public class Project2Aspect {
 	public void catchAllInserts(JoinPoint jp) {
 		logger.info("New Post created");
 	}
-	
-//	@After("execution(* get*Likes*(..))")
-//	public void adviseLikeRetrieval(JoinPoint jp) {
-//		System.out.println("Getting Post Likes");
-//	}
 	
 	@AfterReturning("execution(* newComment(..))")
 	public void adviseNewComment(JoinPoint jp) {
@@ -42,7 +37,7 @@ public class Project2Aspect {
 		try {
 			User u = (User) jp.proceed();
 			if(u != null) {
-				logger.info("User" + u.getFname() + " " + u.getLname() + "Loggedin");
+				logger.info("User" + u.getFname() + " " + u.getLname() + " Loggedin");
 			} else {
 				logger.info("User failed to login");
 			}
@@ -53,10 +48,5 @@ public class Project2Aspect {
 		}
 		return null;
 	}
-	
-//	@AfterThrowing("execution(* **(..))")
-//	public void logAllThrows(JoinPoint jp) {
-//		logger.warn("Throwable in method " + jp.getSignature());
-//	}
 	
 }
