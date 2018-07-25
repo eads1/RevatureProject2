3ac201f5a706893c9d6943b5e1ca44241ab8224e
eads1 <eads1@purdue.edu>
2018-07-25 18:57:28 -0400

    Mo Junit Tests

--- Project2/src/test/java/com/project/testing/UserDaoTest.java
+++ Project2/src/test/java/com/project/testing/UserDaoTest.java
@@ -1,6 +1,7 @@
 package com.project.testing;
 
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,6 +13,10 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.project.dao.CommentDao;
+import com.project.dao.PostDao;
+import com.project.dao.UserDao;
+import com.project.model.Post;
+import com.project.model.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext-test.xml")
@@ -23,12 +28,49 @@ public class UserDaoTest {
 	@Autowired
 	CommentDao cs;
 	
+	@Autowired
+	UserDao ud;
+	
+	@Autowired
+	PostDao pd;
+	
+	Post testPost;
+	User testUser;
+	String email = "testing@gmail.com";
+	
 	@Test
 	@Transactional
 	@Rollback(true)
-	public void testInsert() {
-		String email = "meads30@gmail.com";
-		assertTrue("Comment generation failed", cs.newComment(9, 10, "Junit Testing COmment") > 0);
+	public void testInsertComment() {
+		long res = cs.newComment(9, 10, "Junit Testing COmment");
+		assertTrue("Comment generation failed", res > 0);
 	}
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testNewUser() {
+		User u = new User();
+		u.setEmail(email);
+		ud.insert(u);
+		testUser = ud.selectUserByEmail(email);
+		assertFalse("User not properly created", testUser == null);
+	}
+	
+	@Test
+	@Transactional
+	@Rollback(true)
+	public void testNewPost() {
+		User u = new User();
+		u.setEmail(email);
+		ud.insert(u);
+		testUser = ud.selectUserByEmail(email);
+		Post p = new Post();
+		p.setUserId(testUser);
+		pd.insertPost(p);
+		testPost = pd.getPostsByUserId(testUser.getUserId()).get(0);
+		assertFalse("User not properly created", testPost == null);
+	}
+	
 	
 }
