192c38ef644db398a026605602fe7f2cc9576e30
davidyang11 <yangd5987@gmail.com>
2018-07-26 15:41:04 -0400

    some small tests here and there

--- Angular/src/app/search/search.component.ts
+++ Angular/src/app/search/search.component.ts
@@ -12,9 +12,7 @@ import { switchMap } from 'rxjs/operators';
 export class SearchComponent implements OnInit {
   private users;
   private hasResults = false;
-  constructor(private route: ActivatedRoute,
-    private router: Router,
-    private service: UserService) { }
+  constructor(private route: ActivatedRoute, private router: Router, private service: UserService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
